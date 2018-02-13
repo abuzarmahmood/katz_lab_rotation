9415a347b4bb9b6b6649a37ad0768bbf5ae9de3b
abuzarmahmood <abuzarmahmood@gmail.com>
2018-02-13 13:14:40 -0600

    Added code for std_error bars and plotting unaligned trials

--- combining_correlations.py
+++ combining_correlations.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb 12 09:58:04 2018
-
-@author: abuzarmahmood
-"""
 #Import dat ish
 import numpy as np
 import tables
@@ -48,6 +41,16 @@ for dir_name in dirs:
 hf5.close()
 unique_states = np.unique(unique_states)
 
+# Make indexing vector to pull out correaltion from ancillary analysis array
+window_size = 250
+step_size = 25
+t_length = 7000
+time_vec = np.arange(0,t_length-window_size+step_size,step_size)
+pre_stim_time = 2000
+middle_time = (700+1200)/2
+flank_window = 500
+index_vec = np.where((time_vec>=pre_stim_time+middle_time-flank_window)*(time_vec<=pre_stim_time+middle_time+flank_window))[0]
+
 # Now run through the directories, and pull out the data
 # Going to loop through each file and each model of the HMM (different number 
 # of states) with on and off states separately
@@ -56,10 +59,12 @@ unique_states = np.unique(unique_states)
 for num_state in unique_states:
     
     r_pearson_off = []
-    p_pearson_off = []
+    r_pearson_off_scram = []
+    #p_pearson_off = []
     
     r_pearson_on = []
-    p_pearson_on = []
+    r_pearson_on_scram = []
+    #p_pearson_on = []
     
     num_units_on = 0
     num_units_off = 0
@@ -80,9 +85,13 @@ for num_state in unique_states:
         
             # Pull the data from the /ancillary_analysis node
             exec('r_pearson_off.append(hf5.root.spike_trains.multinomial_hmm_results.laser_off.states_%i.r_pearson[:])' % num_state)
-            exec('p_pearson_off.append(hf5.root.spike_trains.multinomial_hmm_results.laser_off.states_%i.p_pearson[:])' % num_state)
+            #exec('p_pearson_off.append(hf5.root.spike_trains.multinomial_hmm_results.laser_off.states_%i.p_pearson[:])' % num_state)
             exec('r_pearson_on.append(hf5.root.spike_trains.multinomial_hmm_results.laser_on.states_%i.r_pearson[:])' % num_state)
-            exec('p_pearson_on.append(hf5.root.spike_trains.multinomial_hmm_results.laser_on.states_%i.p_pearson[:])' % num_state)
+            #exec('p_pearson_on.append(hf5.root.spike_trains.multinomial_hmm_results.laser_on.states_%i.p_pearson[:])' % num_state)
+            
+            # Append r_pearson from the ancillary analysis of all files
+            r_pearson_off_scram.append(np.ndarray.transpose(hf5.root.ancillary_analysis.r_pearson[0,:]))
+            r_pearson_on_scram.append(np.ndarray.transpose(hf5.root.ancillary_analysis.r_pearson[1,:]))
             ## Also maintain a counter of the number of units in the analysis
             exec('num_units_on += hf5.root.spike_trains.multinomial_hmm_results.laser_on.states_%i.p_pearson.shape[0]' % num_state)
             exec('num_units_off += hf5.root.spike_trains.multinomial_hmm_results.laser_off.states_%i.p_pearson.shape[0]' % num_state)
@@ -92,23 +101,44 @@ for num_state in unique_states:
             
     if len(unique_states)==1:
         r_pearson_off = r_pearson_off[0]
-        p_pearson_off = p_pearson_off[0]
+        #p_pearson_off = p_pearson_off[0]
         r_pearson_on = r_pearson_on[0]
-        p_pearson_on = p_pearson_on[0]
+        #p_pearson_on = p_pearson_on[0]
+        r_pearson_off_scram = r_pearson_off_scram[0]
+        r_pearson_on_scram = r_pearson_on_scram[0]
     else:
         r_pearson_off = np.concatenate(r_pearson_off,0)
-        p_pearson_off = np.concatenate(p_pearson_off,0)
+        #p_pearson_off = np.concatenate(p_pearson_off,0)
         r_pearson_on = np.concatenate(r_pearson_on,0)
-        p_pearson_on = np.concatenate(p_pearson_on,0)
-            
+        #p_pearson_on = np.concatenate(p_pearson_on,0)
+        r_pearson_off_scram = np.concatenate(r_pearson_off_scram,0)
+        r_pearson_on_scram = np.concatenate(r_pearson_on_scram,0)
+        
+    #Index the unalgined correaltions by the index_vec
+    r_pearson_off_scram = r_pearson_off_scram[:,index_vec]
+    r_pearson_on_scram = r_pearson_on_scram[:,index_vec]
+
+        
     fig = plt.figure()
     plt.title('Correlation, Off trials , Model states =' + str(num_state))
-    plt.plot(np.mean(r_pearson_off[:, :]**2, axis = 0))
+    #Plot aligned correlation
+    #aligned, = plt.plot(np.mean(r_pearson_off[:, :]**2, axis = 0))
+    plt.errorbar(range(r_pearson_off.shape[1]),np.mean(r_pearson_off[:, :]**2, axis = 0),yerr=np.std(r_pearson_off[:,:]**2,axis = 0)/np.sqrt(r_pearson_off.shape[0]),label = 'Aligned')
+    #Plot unaligned correlation
+    #unaligned, = plt.plot(np.mean(r_pearson_off_scram[:, :]**2, axis = 0)
+    plt.errorbar(range(r_pearson_off_scram.shape[1]),np.mean(r_pearson_off_scram[:, :]**2, axis = 0),yerr=np.std(r_pearson_off_scram[:, :]**2,axis = 0)/np.sqrt(r_pearson_off_scram.shape[0]),label = 'Unaligned')
+    fig.legend()
     fig.savefig(plot_dir + '/off_correlation_states%i.png' % num_state)
     plt.close('all')
     
     fig = plt.figure()
     plt.title('Correlation, On trials , Model states =' + str(num_state))
-    plt.plot(np.mean(r_pearson_on[:, :]**2, axis = 0))
+    #Plot aligned correlation
+    #plt.plot(np.mean(r_pearson_on[:, :]**2, axis = 0))
+    plt.errorbar(range(r_pearson_on.shape[1]),np.mean(r_pearson_on[:, :]**2, axis = 0),yerr=np.std(r_pearson_on[:,:]**2,axis = 0)/np.sqrt(r_pearson_on.shape[0]), label = 'Aligned')
+    #Plot unaligned correlation
+    #plt.plot(np.mean(r_pearson_on_scram[:, :]**2, axis = 0))
+    plt.errorbar(range(r_pearson_on_scram.shape[1]),np.mean(r_pearson_on_scram[:, :]**2, axis = 0),yerr=np.std(r_pearson_on_scram[:, :]**2,axis = 0)/np.sqrt(r_pearson_on_scram.shape[0]), label = 'Unaligned')
+    fig.legend()
     fig.savefig(plot_dir + '/on_correlation_states%i.png' % num_state)
     plt.close('all')
